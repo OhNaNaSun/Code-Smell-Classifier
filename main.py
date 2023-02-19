d5e1bdbc4534e6d8da15974d3aa2ff8a25efaf2a
OhNaNaSun <k626541201@hotmail.com>
2023-02-18 17:07:52 -0800

    Refactor

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-from scipy.io import arff
 import pandas as pd
-from sklearn import preprocessing
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.impute import SimpleImputer
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import accuracy_score, f1_score
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
-from sklearn.preprocessing import StandardScaler
 import signal
 import sys
+from train_models import train_model
 
 
 def signal_handler(signal, frame):
@@ -25,7 +16,6 @@ signal.signal(signal.SIGINT, signal_handler)
 
 class Main:
     def __init__(self):
-        self.data = None
         self.model_dict = {
             "decision_tree": {
                 "classifier": DecisionTreeClassifier(),
@@ -36,25 +26,25 @@ class Main:
                 },
                 "results": {},
             },
-            "naive_bayes": {
-                "classifier": GaussianNB(),
-                "param_grid": {"var_smoothing": np.logspace(0, -9, num=100)},
-                "results": {},
-            },
-            "k-neighbors": {
-                "classifier": KNeighborsClassifier(n_neighbors=5),
-                "results": {},
-                "param_grid": {
-                    "n_neighbors": [3, 5, 7],
-                    "weights": ["uniform", "distance"],
-                    "algorithm": ["ball_tree", "kd_tree", "brute"],
-                    "leaf_size": [10, 20, 30],
-                    "p": [1, 2],
-                },
-            },
+            # "naive_bayes": {
+            #     "classifier": GaussianNB(),
+            #     "param_grid": {"var_smoothing": np.logspace(0, -9, num=100)},
+            #     "results": {},
+            # },
+            # "k-neighbors": {
+            #     "classifier": KNeighborsClassifier(n_neighbors=5),
+            #     "results": {},
+            #     "param_grid": {
+            #         "n_neighbors": [3, 5, 7],
+            #         "weights": ["uniform", "distance"],
+            #         "algorithm": ["ball_tree", "kd_tree", "brute"],
+            #         "leaf_size": [10, 20, 30],
+            #         "p": [1, 2],
+            #     },
+            # },
         }
 
-        self.smells = {
+        self.smell_files = {
             "feature_envy": "feature-envy.arff",
             "data_class": "data-class.arff",
             "god_class": "god-class.arff",
@@ -63,78 +53,21 @@ class Main:
         self.selected_model = None
         self.selected_smell_types = None
 
-    def _load_data(self, file_path):
-        data = arff.loadarff("A1-files/" + file_path)
-        # TODO: normalize data
-        df = pd.DataFrame(data[0])
-        X = df.iloc[:, :-1].values
-        Y_data = df.iloc[:, -1].values
-
-        encoder = preprocessing.LabelEncoder()
-
-        y = encoder.fit_transform(Y_data)
-
-        # tree_clf.fit(X, y)
-
-        X_copy = df.iloc[:, :-1].copy()
-
-        imputer = SimpleImputer(strategy="most_frequent")
-
-        new_X = imputer.fit_transform(X_copy)
-
-        new_X_df = pd.DataFrame(new_X, columns=X_copy.columns, index=X_copy.index)
-
-        X_train, X_test, y_train, y_test = train_test_split(
-            new_X, y, test_size=0.15, random_state=42
-        )
-        scaler = StandardScaler()
-        X_train_norm = scaler.fit_transform(X_train)
-        X_test_norm = scaler.transform(X_test)
-        self.X_train = X_train
-        self.X_test = X_test
-        # normalize data
-        self.X_train = X_train_norm
-        self.X_test = X_test_norm
-        self.y_train = y_train
-        self.y_test = y_test
-
-        return X_train, X_test, y_train, y_test
-
-    def train_model(self, model_name):
-        current_model = self.model_dict[model_name]
-        tree_clf = current_model["classifier"]
-        param_grid = current_model["param_grid"]
-        tree_clf.fit(self.X_train, self.y_train)
-        grid_search = GridSearchCV(
-            tree_clf, param_grid, cv=10, scoring="accuracy", return_train_score=True
-        )
-
-        grid_search.fit(self.X_train, self.y_train)
-
-        best_model = grid_search.best_estimator_
-
-        tree_clf.fit(self.X_train, self.y_train)
-        # best_model = grid_search.best_estimator_
-
-        # tree_clf.fit(X_train, y_train)
-
-        train_f1 = f1_score(self.y_train, best_model.predict(self.X_train))
-        train_accuracy = accuracy_score(self.y_train, best_model.predict(self.X_train))
-        test_f1 = f1_score(self.y_test, best_model.predict(self.X_test))
-        test_accuracy = accuracy_score(self.y_test, best_model.predict(self.X_test))
-        return test_accuracy, test_f1, train_accuracy, train_f1
-
     def process_trainings(self):
         model_names = self.model_dict.keys()
         for model in model_names:
-            for smell_type in list(self.smells.keys()):
-                self._load_data(self.smells[smell_type])
+            for smell_type in list(self.smell_files.keys()):
+                current_model = self.model_dict[model]
                 (
                     accuracy,
                     f1_score_value,
                     train_accuracy,
                     train_f1_score_value,
-                ) = self.train_model(model)
+                ) = train_model(
+                    self.smell_files[smell_type],
+                    current_model["classifier"],
+                    current_model["param_grid"],
+                )
                 self.model_dict[model]["results"][smell_type] = {
                     "accuracy": accuracy,
                     "f1_score_value": f1_score_value,
@@ -162,6 +95,42 @@ class Main:
             print(df)
             print("------------------")
 
+    def user_input_selection(self, prompt, options, is_multiple_selection):
+        options.append("exit")
+        while True:
+            print(prompt)
+            for i, option in enumerate(options):
+                print(f"{i + 1}. {option}")
+
+            user_input = input("Enter the number of your selection:")
+
+            try:
+                if user_input == str(len(options)):
+                    sys.exit(0)
+
+                if is_multiple_selection:
+                    user_input = [int(x) for x in user_input.split(",")]
+                    for i in user_input:
+                        if i < 1 or i > len(options[:-1]):
+                            raise ValueError
+                else:
+                    user_input = int(user_input)
+                    if user_input < 1 or user_input > len(options[:-1]):
+                        raise ValueError
+
+            except ValueError:
+                print("Invalid input. Please try again.")
+                continue
+
+            if is_multiple_selection:
+                selected_options = [options[:-1][i - 1] for i in user_input]
+                print(f"You selected: {', '.join(selected_options)}\n")
+                return selected_options
+            else:
+                selected_option = options[:-1][user_input - 1]
+                print(f"You selected: {selected_option}\n")
+                return selected_option
+
     def print_selected_results(self):
         model = self.selected_model
         df_results = []
@@ -174,82 +143,55 @@ class Main:
             printed_result.columns = ["Smell Type", "accuracy", "f1_score"]
             df_results.append(printed_result)
         printed_result = pd.concat(df_results, axis=0)
+        print(
+            "The accuracy and the F1-score of the test set for your selected smells:\n"
+        )
         print(printed_result)
+        print("------------------------------------")
 
-    def select_smells(self, callback):
-        options = list(self.smells.keys())
-        selection = [-1]
-        while not all(1 <= x <= len(options) for x in selection):
-            print("Please select one or more options (comma-separated):")
-            for i, option in enumerate(options):
-                print(f"{i + 1}. {option}")
-
-            selection = input("Enter the numbers of your selections: ")
-
-            try:
-                selection = [int(x.strip()) for x in selection.split(",")]
-                if not all(1 <= x <= len(options) for x in selection):
-                    print("Invalid selection. Please try again")
-                    selection = [-1]
-            except ValueError:
-                print("Invalid input. Please try again.")
-                selection = [-1]
-
-        selected_options = [options[x - 1] for x in selection]
-        self.selected_smell_types = selected_options
-        print(f'You selected: {", ".join(selected_options)}')
-        callback()
+    def select_smells(self):
+        options = list(self.smell_files.keys())
+        self.selected_smell_types = self.user_input_selection(
+            "Select one or more code smells or exit: ",
+            options,
+            True,
+        )
 
     def compare_or_retrain(self):
         # print("Compare or Retrain")
-        options = ["Compare", "Retrain"]
-        selection = -1
-        while not 1 <= selection <= len(options):
-            for i, option in enumerate(options):
-                print(f"{i + 1}. {option}")
-
-            selection = input("Enter the number of your selections: ")
-            try:
-                selection = int(selection)
-                if not 1 <= selection <= len(options):
-                    selection = -1
-                    print("Invalid selection.")
-            except ValueError:
-                print("Invalid input.")
-                selection = -1
-        print(f'You selected "{options[selection - 1]}"')
-        if selection == 1:
+        options = ["compare test and training", "retrain"]
+        selection = self.user_input_selection(
+            "Select the option you want to do next or exit: ", options, False
+        )
+        if selection == options[0]:
             # Compare Test and Training
             self.print_compared_result()
+            selection = self.user_input_selection(
+                "Select the option you want to do next or exit: ", ["retrain"], False
+            )
+            if selection == "retrain":
+                self.start_interaction()
         else:
             # Retrain
-            self.enter_model_type(self.start_train)
+            self.start_interaction()
 
-    def enter_model_type(self, callback):
+    def enter_model_type(self):
         options = list(self.model_dict.keys())
-        selection = -1
-        while not 1 <= selection <= len(options):
-            for i, option in enumerate(options):
-                print(f"{i + 1}. {option}")
-            selection = input("Enter the number of your selection: ")
-            try:
-                selection = int(selection)
-                if not 1 <= selection <= len(options):
-                    print("Invalid option. Please try again.")
-            except ValueError:
-                selection = -1
-                print("Invalid option. Please try again.")
-        print(f'You selected "{options[selection - 1]}"')
-        self.selected_model = options[selection - 1]
-        callback()
+        self.selected_model = self.user_input_selection(
+            "Select one trained model or exit: ",
+            options,
+            False,
+        )
 
-    def start_train(self):
-        self.select_smells(self.print_selected_results)
+    def start_interaction(self):
+        self.enter_model_type()
+        self.select_smells()
+        self.print_selected_results()
         self.compare_or_retrain()
 
     def run_application(self):
         self.process_trainings()
-        self.enter_model_type(self.start_train)
+        self.start_interaction()
 
 
 if __name__ == "__main__":
--- train_models.py
+++ train_models.py
@@ -1,78 +1,57 @@
 from scipy.io import arff
+from sklearn.preprocessing import StandardScaler
 import pandas as pd
 from sklearn import preprocessing
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score, f1_score
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neighbors import KNeighborsClassifier
-import numpy as np
 
 
-def train_model(self):
-    tree_clf = KNeighborsClassifier(n_neighbors=5)
-    tree_clf.fit(self.X_train, self.y_train)
+def process_data(file_path):
+    data = arff.loadarff("A1-files/" + file_path)
+    df = pd.DataFrame(data[0])
+    X = df.iloc[:, :-1].values
+    Y_data = df.iloc[:, -1].values
+    encoder = preprocessing.LabelEncoder()
+    y = encoder.fit_transform(Y_data)
+    X_copy = df.iloc[:, :-1].copy()
+    imputer = SimpleImputer(strategy="most_frequent")
+    new_X = imputer.fit_transform(X_copy)
+    new_X_df = pd.DataFrame(new_X, columns=X_copy.columns, index=X_copy.index)
+    X_train, X_test, y_train, y_test = train_test_split(
+        new_X, y, test_size=0.15, random_state=42
+    )
+    scaler = StandardScaler()
+    X_train_norm = scaler.fit_transform(X_train)
+    X_test_norm = scaler.transform(X_test)
+    # X_train = X_train
+    # X_test = X_test
+    # normalize data
+    X_train = X_train_norm
+    X_test = X_test_norm
+    y_train = y_train
+    y_test = y_test
 
-    depths = np.arange(10, 21)
+    return X_train, X_test, y_train, y_test
 
-    num_leafs = [1, 5, 10, 20, 50, 100]
 
-    param_grid = {
-        "criterion": ["gini", "entropy"],
-        "max_depth": depths,
-        "min_samples_leaf": num_leafs,
-    }
-
+def train_model(smell_file_path, classifier, param_grid):
+    X_train, X_test, y_train, y_test = process_data(smell_file_path)
+    tree_clf = classifier
+    tree_clf.fit(X_train, y_train)
     grid_search = GridSearchCV(
         tree_clf, param_grid, cv=10, scoring="accuracy", return_train_score=True
     )
 
-    grid_search.fit(self.X_train, self.y_train)
+    grid_search.fit(X_train, y_train)
 
     best_model = grid_search.best_estimator_
+    tree_clf.fit(X_train, y_train)
 
-    tree_clf.fit(self.X_train, self.y_train)
-    # best_model = grid_search.best_estimator_
-
-    # tree_clf.fit(X_train, y_train)
-
-    train_f1 = f1_score(self.y_train, tree_clf.predict(self.X_train))
-    train_accuracy = accuracy_score(self.y_train, tree_clf.predict(self.X_train))
-    test_f1 = f1_score(self.y_test, tree_clf.predict(self.X_test))
-    test_accuracy = accuracy_score(self.y_test, tree_clf.predict(self.X_test))
-
-    # Training set accuracy
-
-    print("The training accuracy is: ")
-
-    print(
-        "DT default hyperparameters: "
-        + str(accuracy_score(self.y_train, tree_clf.predict(self.X_train)))
-    )
-
-    print(
-        "DT optimized hyperparameters: "
-        + str(accuracy_score(self.y_train, best_model.predict(self.X_train)))
-    )
-
-    # Test set accuracy
-
-    print("\nThe test accuracy is: ")
-
-    print(
-        "DT default hyperparameters: "
-        + str(accuracy_score(self.y_test, tree_clf.predict(self.X_test)))
-    )
-
-    print(
-        "DT optimized hyperparameters: "
-        + str(accuracy_score(self.y_test, best_model.predict(self.X_test)))
-    )
-    # print(test_accuracy, test_f1, train_accuracy, train_f1)
-    # TODO: grid search
-    # return test_accuracy, test_f1, train_accuracy, train_f1
+    train_f1 = f1_score(y_train, best_model.predict(X_train))
+    train_accuracy = accuracy_score(y_train, best_model.predict(X_train))
+    test_f1 = f1_score(y_test, best_model.predict(X_test))
+    test_accuracy = accuracy_score(y_test, best_model.predict(X_test))
+    return test_accuracy, test_f1, train_accuracy, train_f1
